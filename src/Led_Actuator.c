3187ed3ca59fbdb9585141482fbc76058e0df549
Nishanth256555 <56690808+nishanthsri007@users.noreply.github.com>
2021-04-28 12:56:55 +0530

    Update Led_Actuator.c

--- src/Led_Actuator.c
+++ src/Led_Actuator.c
@@ -2,10 +2,10 @@
 void Led_Actuator()
 {
 
-DDRB|=(1<<PB0);
-DDRD&=~(1<<PD0);
-PORTD|=(1<<PD0);
-DDRD&=~(1<<PD4);
-PORTD|=(1<<PD4);
+DDRB|=(1<<PB0);       // Set B0=1 for LED
+DDRD&=~(1<<PD0);      // Clear bit
+PORTD|=(1<<PD0);      // Set D0=1 for switch
+DDRD&=~(1<<PD4);      // Clear bit
+PORTD|=(1<<PD4);      // Set D4=1 for switch
 
 }
